448bd553f5ab86e0ccf9173d124185e1a3d8acd2
Ștefan Neacșu-Miclea <91871146+stefnmUBB@users.noreply.github.com>
2022-12-03 20:13:24 +0200

    Add files via upload

--- 6/test_insertions.sql
+++ 6/test_insertions.sql
@@ -18,10 +18,46 @@ END GO
 SELECT * FROM Tests GO
 
 CREATE OR ALTER PROCEDURE AddTestTables AS BEGIN
+	DELETE FROM TestTables
+
 	INSERT INTO TestTables (TestID, TableID, NoOfRows, Position) VALUES
 		(1, 2, 50, 1),  -- Materie prima moneda
 		(1, 3, 50, 1),  -- Material moneda
 		(1, 1, 20, 2),  -- Compozitie moneda
 		(1, 4, 20, 2),  -- Tip moneda
 		(1, 5, 100, 3)  -- Tip moneda
+
+	INSERT INTO TestTables (TestID, TableID, NoOfRows, Position) VALUES
+		(2, 2, 300, 1), -- Materie Prima Moneda		
+		(2, 1, 60, 2)  -- Compozitie moneda
+
+	INSERT INTO TestTables (TestID, TableID, NoOfRows, Position) VALUES
+		(3, 1, 20, 1) -- Compozitie Moneda
+
+	SELECT * FROM TestTables
+	SELECT * FROM TestViews
+	
 END GO
+
+CREATE OR ALTER PROCEDURE AddViews AS BEGIN
+	INSERT INTO Views (Name) VALUES 
+		('dbo.vw_Exemplare'),
+		('dbo.vw_Compozitii'),
+		('dbo.vw_Monede2000')
+END GO
+
+-- SELECT * FROM Views
+
+CREATE OR ALTER PROCEDURE AddTestViews AS BEGIN
+	INSERT INTO TestViews (TestID, ViewID) VALUES 
+		(1, 1),
+		(1, 2),
+
+		(2, 3),
+
+		(3, 1),
+		(3, 2),
+		(3, 3)
+END GO
+
+-- SELECT * FROM TestViews
